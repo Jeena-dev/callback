2e03e0b33a088b230895e4e50d04e5511ff30d71
Jeenadevgit config --global user.name jeenaDev <jeenanoori92@gmail.com>
2024-08-23 16:10:56 -0400

    add example for setTimeOut and setInterval func

--- js/app.js
+++ js/app.js
@@ -18,12 +18,83 @@ function greet(name){
 
 
 
- const hof = (callback)=>{
-    const userName = prompt("Please enter your name?")
-    callback(userName)
- }
+//  const hof = (callback)=>{
+//     const userName = prompt("Please enter your name?")
+//     callback(userName)
+//  }
 
-hof(greet);
+// hof(greet);
+
+
+//Synchronous:in order
+//console.log(1);
+//console.log(2); first 1 run then 2 run.
+
+
+//Asynchronous:order is not matter.u can run be4/after dont run 1 wait ku 2 first run ku baz 1.
+// move on when it is ready the call it
+
+
+//1min=60sec .1sec=1000meli second
+//setTimeOUt accept 2 things hof and a time.
+let first = ()=>console.log("Hello,First");
+let second =()=>{
+    setTimeout(function(){
+        console.log("Hello,second")
+    },4000)
+
+}
+
+
+
+first();
+second();
+console.log("Hello,I am here.");
+
+
+
+
+const learnGit = ()=> console.log("Hello,Everyone")
+
+setTimeout(learnGit,3000)
+
+
+
+
+
+function asynOperation(callback){
+ console.log("Operation started ...");
+
+setTimeout(()=> {console.log("Hello,Everyone");
+callback();
+},2000);
+
+}
+
+asynOperation(()=>{
+    console.log("callback executed after operation completion");
+})
+
+
+
+
+
+
+
+function displayTime(){
+    console.log("Current Time" +new Date().toLocaleTimeString());
+}
+
+// setInterval(displayTime,3000)
+
+
+
+
+
+
+
+
+
 
 
 
